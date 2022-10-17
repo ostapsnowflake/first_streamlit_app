1abe768946fb1db3c07edb7d4f8c608de6d60756
ostapsnowflake <115396200+ostapsnowflake@users.noreply.github.com>
2022-10-17 22:13:17 +0300

    Update streamlit_app.py
    Added function for inserting data into fruit_load_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,4 +66,8 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfru
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 #Insert value into table on Snowflake
-my_cur.execute("insert into fruit_load_list values ('from streamlit');")
+def insert_row_snowflake (new_fruit):
+   with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('"+new_fruit+"');")
+        return "Thanks for adding" + new_fruit
+   
