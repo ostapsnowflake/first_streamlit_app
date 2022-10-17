802b20294bc138be2467584265303971549edee9
ostapsnowflake <115396200+ostapsnowflake@users.noreply.github.com>
2022-10-17 19:31:34 +0300

    Update streamlit_app.py
    Small re-organization

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title ('My Parents New healthy Diner')
 
 streamlit.header('🥣Breakfast Menu')
@@ -8,7 +13,7 @@ streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
    
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -22,8 +27,6 @@ streamlit.dataframe(fruits_to_show)
 
 #New section
 streamlit.header("Fruityvice Fruit Advice!")
-
-import requests
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
@@ -39,7 +42,6 @@ streamlit.dataframe(fruityvice_normalized)
 #don't run anything past her while we troubkeshoot
 streamlit.stop()
 
-import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
