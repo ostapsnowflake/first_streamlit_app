6529cfa61c37187b7d82dd8c4634b5fc6997bfcb
ostapsnowflake <115396200+ostapsnowflake@users.noreply.github.com>
2022-10-17 19:24:14 +0300

    Update streamlit_app.py
    Insert test row into table

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,6 @@ streamlit.header("The fruit load list contains ")
 streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+#Insert value into table on Snowflake
+my_cur.execute("insert into fruit_load_list values ('from streamlit');")
